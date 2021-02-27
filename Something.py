855e98c21f377ff0f1f3efec213f0ea88ed0d512
dep93 <65921266+dep93@users.noreply.github.com>
2021-02-27 15:36:58 -0500

    Add files via upload

---
+++ Something.py
@@ -0,0 +1,4 @@
+word = input("Input a string capitalizing every other letter")
+print(word.lower())##turns capitalized letters into lowercase and prints
+print(word.capitalize()) ## capitalized first letter and prints
+print(word.swapcase())##flips the case of all letters in word and prints
