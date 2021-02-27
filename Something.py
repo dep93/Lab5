5d7d9be79883987f4ca8cda596c5def15f54dea6
dep93 <65921266+dep93@users.noreply.github.com>
2021-02-27 15:42:22 -0500

    Update Something.py

--- Something.py
+++ Something.py
@@ -1,3 +1,4 @@
+name= input("What is your name?)
 word = input("Input a string capitalizing every other letter")
 print(word.lower())##turns capitalized letters into lowercase and prints
 print(word.capitalize()) ## capitalized first letter and prints
